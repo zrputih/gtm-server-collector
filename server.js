04b9120f1d4d22b899b516b02f4c68720ae83192
zrputih <zorro4dputih@gmail.com>
2025-04-27 21:09:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
-
 app.use(express.json());
 
 app.post('/collect', (req, res) => {
-  console.log('Received event:', JSON.stringify(req.body, null, 2));
-  res.status(200).send('Event received!');
+  console.log('Received event:', req.body);
+  res.status(200).send('Event Received');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from GTM Server Collector!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
